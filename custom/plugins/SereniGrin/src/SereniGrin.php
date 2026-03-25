2df467aaf3da49678ddabbb916ab3ea02fe0d5c4
happy_dws <happy@dolphinwebsolutions.com>
2026-03-25 18:55:44 +0530

    Added SereniGrin custom theme and UI changes

---
+++ custom/plugins/SereniGrin/src/SereniGrin.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace SereniGrin;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class SereniGrin extends Plugin implements ThemeInterface
+{
+}
